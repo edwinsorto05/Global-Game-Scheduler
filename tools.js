5a887becbb24ffe699127ffff3d99bbdc90c4f9c
Edwin Sorto Rosales <109838799+edwinsorto05@users.noreply.github.com>
2026-05-11 15:56:54 -0400

    Add performance timing to convertToMatrix function
    Added timing for the convertToMatrix function to measure performance.

--- tools.js
+++ tools.js
@@ -3,50 +3,42 @@ function getOffset(){
     var offsetHour = offset/60;
     return offsetHour
 }
-
 function convertToMatrix(){
+    console.time("scheduler");
     const data = { /* your full JSON here */ };
     const people = data.people;
-
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const hours = Array.from({ length: 24 }, (_, i) => i);
-
     // Create 7 rows × 24 columns grid
     const grid = Array.from({ length: 7 }, () =>
         Array.from({ length: 24 }, () => [])
     );
-
     // Fill grid
     people.forEach(person => {
         person.availability.forEach(slot => {
             const dayIndex = days.indexOf(slot.day);
-
         slot.time.forEach(hour => {
             grid[dayIndex][hour].push(person.name);
             });
         });
     });
-
+    console.timeEnd("scheduler");
     return(grid);
 }
-
 class UnionFind {
     constructor(n) {
         this.parent = new Array(n).fill(0).map((_, i) => i);
         this.rank = new Array(n).fill(0);
     }
-
     find(x) {
         if (this.parent[x] !== x) {
             this.parent[x] = this.find(this.parent[x]);
         }
         return this.parent[x];
     }
-
     union(a, b) {
         let rootA = this.find(a);
         let rootB = this.find(b);
-
         if (rootA !== rootB) {
             if (this.rank[rootA] < this.rank[rootB]) {
                 this.parent[rootA] = rootB;
@@ -59,5 +51,4 @@ class UnionFind {
         }
     }
 }
-
 export {getOffset, convertToMatrix, UnionFind};
